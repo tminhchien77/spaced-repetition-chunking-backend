4c21b0221a49edd2fc731f1ac07583b2e7fdf0f2
Tran Minh Chien <tminhchien77@gmail.com>
2026-01-19 11:23:27 +0700

    add env

--- infra/db/connection.py
+++ infra/db/connection.py
@@ -1,13 +1,39 @@
+# from psycopg2.pool import SimpleConnectionPool
+
+# pool = SimpleConnectionPool(
+#     minconn=1,
+#     maxconn=10,
+#     user="tranminhchien",
+#     password="tranminhchien",
+#     host="localhost",
+#     port=5432,
+#     database="src_system"
+# )
+
+# def get_connection():
+#     conn = pool.getconn()
+#     conn.autocommit = True
+#     return conn
+
+# def close_connection(conn):
+#     pool.putconn(conn)
+
+import os
 from psycopg2.pool import SimpleConnectionPool
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+ENV = os.getenv("ENV", "local")
+
+sslmode = "require" if ENV == "production" else "disable"
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
 pool = SimpleConnectionPool(
     minconn=1,
     maxconn=10,
-    user="tranminhchien",
-    password="tranminhchien",
-    host="localhost",
-    port=5432,
-    database="src_system"
+    dsn=DATABASE_URL,
+    sslmode=sslmode
 )
 
 def get_connection():
